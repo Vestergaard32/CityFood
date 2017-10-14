772e96510172f55a0a288156fb9f6a8ecc6e32b0
Vestergaard <Vestergaard32@gmail.com>
2017-10-14 15:33:10 +0200

    finished

--- CityFood/app/src/main/java/com/android/vestergaard/cityfood/FoodRandomizer.java
+++ CityFood/app/src/main/java/com/android/vestergaard/cityfood/FoodRandomizer.java
@@ -1,4 +1,106 @@
 package com.android.vestergaard.cityfood;
 
+import java.util.Random;
+
 public class FoodRandomizer {
+    public static String[] meatArray = new String[]{"Chicken", "Turkey", "Pork", "Meatballs", "Duck", "Cud", "Salmon", "Flounder", "Tofu", "Veal", "Kobe Beef", "Shrimp", "Steak", "Pheasant", "Venison", "Grasshopper"};
+    public static String[] spiceArray = new String[]{"Salt", "Pepper", "Curry", "Paprika", "Cinnamon", "Ghost Pepper", "Basil", "Oregano", "Garlic", "Sugar", "Redwine"};
+    public static String[] fruitAndVegetablesArray = new String[]{"Apple", "Cucumber", "Tomato", "Iceberg Salat", "Bell Pepper", "cabbage", "White Cabbage", "Kale", "Brussel Sprouts", "Broccoli", "Radishes", "Sweet Potato", "Corn", "Peas", "Grapes"};
+    public static String[] baseArray = new String[]{"Potatos", "Bulgur", "Pasta", "Rice", "Lentils", "Pearl Barley", "Quinoa", "Cous Cous", "Lasagna Sheets"};
+    public static String[] sauceArray = new String[]{"Bordelais Sauce", "Gravy sauce", "Bernaise Sauce", "Favoritsovs", "SweetNSour Sauce", "Curry Sauce", "Monai Sauce"};
+    public static String[] sideDishArray = new String[]{"Garlic Bread", "Baguette", "Caviar", "Crackers", "Cheese", "Chips", "Grissini", "Croutons", "Naan", "Fries"};
+
+    public static String[] cookingMethod = new String[] {"Fry", "Boil", "Bake", "Grill", "Deep fry", "Roast"};
+    public static String[] baseCookingMethod = new String[] {"fry", "bake", "grill", "deep fry", "roast"};
+
+    public static String[] cookingTime = new String[] {"seconds", "minutes"};
+    public static String[] veggieMethod = new String[] {"chopped", "steamed", "grilled", "boiled", "fried", "roasted"};
+
+    public String getBaseCookingMethod()
+    {
+        Random randomInteger = new Random();
+        int randomlyGeneratedIndex = randomInteger.nextInt(baseCookingMethod.length);
+        return baseCookingMethod[randomlyGeneratedIndex];
+    }
+
+    public String getCookingMethod()
+    {
+        Random randomInteger = new Random();
+        int randomlyGeneratedIndex = randomInteger.nextInt(cookingMethod.length);
+        return cookingMethod[randomlyGeneratedIndex];
+    }
+
+    public String getCookingUnit()
+    {
+        Random randomInteger = new Random();
+        int randomlyGeneratedIndex = randomInteger.nextInt(cookingTime.length);
+        return cookingTime[randomlyGeneratedIndex];
+    }
+
+    public String getVeggiestuff()
+    {
+        Random randomInteger = new Random();
+        int randomlyGeneratedIndex = randomInteger.nextInt(veggieMethod.length);
+        return veggieMethod[randomlyGeneratedIndex];
+    }
+
+    public int getCookingTime()
+    {
+        Random randomInteger = new Random();
+        return randomInteger.nextInt(121);
+    }
+
+    public int getWeight()
+    {
+        Random randomInteger = new Random();
+        return randomInteger.nextInt(1500);
+    }
+
+    public String selectRandomFrom(String[] a){
+        Random randomInteger = new Random();
+        int randomlyGeneratedIndex = randomInteger.nextInt(a.length);
+        return a[randomlyGeneratedIndex];
+    }
+
+    public String getMeat()
+    {
+        Random randomInteger = new Random();
+        int randomlyGeneratedIndex = randomInteger.nextInt(meatArray.length);
+        return meatArray[randomlyGeneratedIndex];
+    }
+
+    public String getSpice()
+    {
+        Random randomInteger = new Random();
+        int randomlyGeneratedIndex = randomInteger.nextInt(spiceArray.length);
+        return spiceArray[randomlyGeneratedIndex];
+    }
+
+    public String getFruitNVeggie()
+    {
+        Random randomInteger = new Random();
+        int randomlyGeneratedIndex = randomInteger.nextInt(fruitAndVegetablesArray.length);
+        return fruitAndVegetablesArray[randomlyGeneratedIndex];
+    }
+
+    public String getBase()
+    {
+        Random randomInteger = new Random();
+        int randomlyGeneratedIndex = randomInteger.nextInt(baseArray.length);
+        return baseArray[randomlyGeneratedIndex];
+    }
+
+    public String getSauce()
+    {
+        Random randomInteger = new Random();
+        int randomlyGeneratedIndex = randomInteger.nextInt(sauceArray.length);
+        return sauceArray[randomlyGeneratedIndex];
+    }
+
+    public String getSideDish()
+    {
+        Random randomInteger = new Random();
+        int randomlyGeneratedIndex = randomInteger.nextInt(sideDishArray.length);
+        return sideDishArray[randomlyGeneratedIndex];
+    }
 }
--- CityFood/app/src/main/java/com/android/vestergaard/cityfood/MainActivity.java
+++ CityFood/app/src/main/java/com/android/vestergaard/cityfood/MainActivity.java
@@ -1,7 +1,11 @@
 package com.android.vestergaard.cityfood;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.Typeface;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -12,15 +16,18 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     Button ShareButton;
     Button FeedMeButton;
     TextView RecipeView;
+    FoodRandomizer randomizer;
 
     String type = "image/*";
-    String filename = "/shittyFood.jpg";
-    String mediaPath = Environment.getExternalStorageDirectory() + filename;
-
+    String mediaPath;
+    Uri imageUri;
     private final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
@@ -31,6 +38,11 @@ public class MainActivity extends AppCompatActivity {
         ShareButton = (Button) findViewById(R.id.share_btn);
         FeedMeButton = (Button) findViewById(R.id.feedme_btn);
         RecipeView = (TextView) findViewById(R.id.recipe_textview);
+
+        Date Date = new Date();
+        String filename = "/CityFood_" + Date.getMinutes()+".jpg";
+        mediaPath = Environment.getExternalStorageDirectory() + filename;
+        imageUri = Uri.fromFile(new File(mediaPath));
 
         ShareButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,23 +55,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 FeedMe();
+                MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.citywok);
+                mp.start();
             }
         });
     }
 
-
     public void FeedMe()
     {
-        Toast.makeText(MainActivity.this, "Feed me has been clicked!", Toast.LENGTH_SHORT).show();
-        String text = RecipeView.getText().toString();
-        RecipeView.setText(text+" Feed me more!");
-
+        String recipe = generateRecipe();
+        RecipeView.setText(recipe);
     }
 
     public void Share()
     {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            takePictureIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, imageUri);
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
@@ -69,9 +81,64 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode){
             case REQUEST_IMAGE_CAPTURE:
                 if(resultCode == RESULT_OK){
-                    Bundle extras = data.getExtras();
-                    
+                    createInstagramIntent();
                 }
         }
+    }
+
+    private void createInstagramIntent(){
+
+        // Create the new Intent using the 'Send' action.
+        Intent share = new Intent(Intent.ACTION_SEND);
+
+        // Set the MIME type
+        share.setType(type);
+
+        // Create the URI from the media
+        File file = new File(mediaPath);
+        Uri uri = Uri.fromFile(file);
+
+        // Add the URI to the Intent.
+        share.putExtra(Intent.EXTRA_STREAM, uri);
+        share.setPackage("com.instagram.android");
+
+        // Broadcast the Intent.
+        startActivity(Intent.createChooser(share, "Share to"));
+    }
+
+    private String generateRecipe()
+    {
+        randomizer = new FoodRandomizer();
+        String cookingMethod = randomizer.getCookingMethod();
+        int getCookingTime = randomizer.getCookingTime();
+        String getCookingUnit = randomizer.getCookingUnit();
+        int meatWeight = randomizer.getWeight();
+        int veggieWeight = randomizer.getWeight();
+        String veggieMethod = randomizer.getVeggiestuff();
+        String meat = randomizer.getMeat();
+        String spice1 = randomizer.getSpice();
+        String spice2 = randomizer.getSpice();
+        String vegetable = randomizer.getFruitNVeggie();
+        String base = randomizer.getBase();
+        int baseWeight = randomizer.getWeight();
+        int baseCookingTime = randomizer.getCookingTime();
+        String baseCookingUnit = randomizer.getCookingUnit();
+        String baseCookingMethod = randomizer.getBaseCookingMethod();
+        String sauce = randomizer.getSauce();
+        String sideDish = randomizer.getSideDish();
+
+        String Ingredients = "INGREDIENTS: \n" + meatWeight + " grams of " + meat + "\n" +
+                veggieWeight + " grams of " + vegetable + "\n" +
+                baseWeight + " grams of " + base + "\n" +
+                spice1 + ", " + spice2 + "\n" +
+                sauce + "\n" + sideDish;
+
+        String recipe = cookingMethod+ " the " + meatWeight + " grams of " + meat + " for " + getCookingTime + " " + getCookingUnit + "\n"+
+                "Season the " + meat + " with a sprinkle of " + spice1 + " and a dash of " + spice2 + "\n" +
+                "Boil first and then " + baseCookingMethod + " the " + baseWeight + " grams of " + base + " for " + baseCookingTime + " " + baseCookingUnit + "\n" +
+                "Add " + veggieWeight + " grams of " + veggieMethod + " " + vegetable + "\n"+
+                "Serve fresh with " + sauce + " and a side dish of " + sideDish;
+
+        return Ingredients + "\n\n" + recipe;
     }
 }
